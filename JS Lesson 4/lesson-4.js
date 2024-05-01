ff1f1a78c01c3d08e194b89b72dc6980d80bfc88
Daria Hrebeniuk <144627994+DariaHrebeniuk17@users.noreply.github.com>
2024-05-01 23:01:01 +0300

    Add files via upload

---
+++ JS Lesson 4/lesson-4.js
@@ -0,0 +1,154 @@
+//Функції//
+
+/*function showMessage(name, city) {
+  console.log("Hello, my name is " + name + "I'm from", city);
+}
+
+showMessage("Dasha ", "Kyiv");
+
+showMessage("Olga ", "Lviv");*/
+
+
+/*const userName = "Ann";
+
+
+const sayHi = function () {
+    let message = "My name is ";
+    console.log(message + userName);
+};
+
+sayHi();*/
+
+
+/*function sum (a, b) {
+    return a + b;
+}
+
+let result  = sum(7, 8);
+console.log(result);*/
+
+// const testFunc = function(a, b) {
+//     console.log("a :", a);
+//     console.log("b :", b);
+
+//     return;
+//     a + b;
+// };
+
+// testFunc (5, 8);
+
+
+// const a = function () {
+//     c();
+//     console.log("function a");
+//     b();
+// };
+
+// const b = function () {
+//     console.log("function b");
+// };
+
+// const c = function () {
+//     console.log("function c");
+// };
+
+// a();
+
+// const addName = function (...arg) {
+//    console.log(arg);
+// };
+
+// addName(1, 3, 6);
+
+// addName(1, 3, 6, 99);
+
+// function ask(question, yes, no) {
+//     if (confirm(question)) {
+//         yes();
+//     } else {
+//         no();
+//     }
+// }
+
+// function showOk () {
+//     console.log("U say ok");
+// };
+
+// function showCancel () {
+//     console.log("U say no");
+// };
+
+// ask("Yes or no ?", showOk, showCancel);
+
+
+// const hello = function(name) {
+//     console.log(`Hello, ${name}`);
+// };
+
+// const searchName = function (callback) {
+//     const name = "Ivan";
+//     callback (name);
+// };
+
+// searchName(hello);
+
+// function checkAge(age) {
+//     if(age > 18) {
+//         return console.log("Hello");
+//     }
+//         return console.log("Good bye");
+// };
+
+// checkAge(12);
+
+// function showMovie (age) {
+//     if (age < 21) {
+//         return
+//     } 
+//         return console.log("Go to movie!");
+// }
+
+// showMovie(23); 
+
+// const test = (arg) => {
+//     console.log(arg);
+// };
+
+// test ("Hello");
+
+
+// const showMovie = (age) => {
+//     if (age > 21) {
+//         return console.log("Hello");
+//     } return console.log("Bye");
+// };
+
+// showMovie(12);
+
+// const a = () => {
+//     console.log("a");
+// };
+
+// const b = () => {
+//     console.log("b");
+// };
+
+// const c = () => {
+//     console.log("c");
+//     a();
+//     b();
+// };
+
+// c();
+
+// const addNumber = (number) => {
+//     let summ = Number(number) = 10;
+//     return console.log(summ);
+// };
+
+
+// const numberRef = document.querySelector('input[name ="number"]');
+
+// const buttonRef = document.querySelector("button");
+
+// buttonRef.addEventListener("click", () => addNumber(numberRef.value));
